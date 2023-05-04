f30c4f9f41564d0a3d046394046459eb51228715
Danilo <danilo.silva@msn.com>
2023-05-03 21:54:42 -0300

    Criação do Projeto Inicial

--- IES/Models/ErrorViewModel.cs
+++ IES/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace IES.Models;
-
-public class ErrorViewModel
+namespace IES.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
