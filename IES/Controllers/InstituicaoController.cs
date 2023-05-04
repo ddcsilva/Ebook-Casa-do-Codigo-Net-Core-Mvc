3cc606ff813d3c19fe519f25958bb51184916479
Danilo <danilo.silva@msn.com>
2023-05-03 23:16:58 -0300

    Implementação da Inserção de Dados no Controlador

--- IES/Controllers/InstituicaoController.cs
+++ IES/Controllers/InstituicaoController.cs
@@ -46,4 +46,19 @@ public class InstituicaoController : Controller
     {
         return View(instituicoes);
     }
+
+    // HttpPost: Indica que o método responde a uma requisição do tipo POST
+    // ValidateAntiForgeryToken: Indica que o método requer um token de validação
+    // Definindo a rota para a página de criação de uma Instituição
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Instituicao instituicao)
+    {
+        // Adicionando a Instituição na lista de Instituições
+        instituicoes.Add(instituicao);
+        // Buscando o maior ID da lista de Instituições e incrementando 1
+        instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+        // Redirecionando para a página inicial da Instituição
+        return RedirectToAction("Index");
+    }
 }
