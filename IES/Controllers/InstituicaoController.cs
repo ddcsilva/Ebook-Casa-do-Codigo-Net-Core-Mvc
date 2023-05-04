2f8100083a5b8bf9cc09e8f07b1f5ee680f08078
Danilo <danilo.silva@msn.com>
2023-05-03 22:46:06 -0300

    Implementação da Interação da Action Index com a Visão

--- IES/Controllers/InstituicaoController.cs
+++ IES/Controllers/InstituicaoController.cs
@@ -1,12 +1,49 @@
+using IES.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IES.Controllers;
 
 public class InstituicaoController : Controller
 {
+    // Criando uma lista de Instituições
+    private static IList<Instituicao> instituicoes = 
+        new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "UniParaná",
+                Endereco = "Paraná"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 2,
+                Nome = "UniSanta",
+                Endereco = "Santa Catarina"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 3,
+                Nome = "UniSãoPaulo",
+                Endereco = "São Paulo"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 4,
+                Nome = "UniSulgrandense",
+                Endereco = "Rio Grande do Sul"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 5,
+                Nome = "UniCarioca",
+                Endereco = "Rio de Janeiro"
+            }
+        };
+
     // Definindo a rota para a página inicial da Instituição
     public IActionResult Index()
     {
-        return View();
+        return View(instituicoes);
     }
 }
--- IES/Models/Instituicao.cs
+++ IES/Models/Instituicao.cs
@@ -1,14 +1,16 @@
 namespace IES.Models;
 
+// Classe que representa uma instituição de ensino
 public class Instituicao
 {
+    // Construtor que inicializa as propriedades
     public Instituicao()
     {
         Nome = string.Empty;
         Endereco = string.Empty;
     }
 
-    public long? InsituicaoID { get; set; }
+    public long? InstituicaoID { get; set; }
     public string Nome { get; set; }
     public string Endereco { get; set; }
 }
