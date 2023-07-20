fa7b25a1d8f4e06dfea9e1f44ad671e36851b24d
Danilo Silva <danilo.silva@msn.com>
2023-07-19 23:33:20 -0300

    Reset

--- IES/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IES.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
