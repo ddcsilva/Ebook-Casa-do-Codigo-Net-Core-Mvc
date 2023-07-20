9dc0515289c00439d7c3eabfe3de7da5a0742c4a
Danilo Silva <danilo.silva@msn.com>
2023-07-19 23:55:51 -0300

    Criação da Classe de Domínio para Instituições de Ensino

---
+++ src/IES/Models/Instituicao.cs
@@ -0,0 +1,8 @@
+namespace IES.Models;
+
+public class Instituicao
+{
+    public long? InstituicaoID { get; set; }
+    public string? Nome { get; set; }
+    public string? Endereco { get; set; }
+}
